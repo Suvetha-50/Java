9a226a4341ea5cc6d5ab98a1eb8569b9775e056d
Suvetha T <149592628+Suvetha-50@users.noreply.github.com>
2025-01-06 21:29:22 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,31 @@
+import java.io.*; 
+import java.net.*; 
+import java.lang.*; 
+public class Server{
+public static void main(String args[])throws IOException{
+ServerSocket s=null; 
+String line; 
+DataInputStream is; 
+PrintStream ps; 
+Socket c=null;
+try{
+s=new ServerSocket(8080);
+}
+catch(IOException e){
+System.out.println(e);
+}
+try{
+c=s.accept();
+is=new DataInputStream(c.getInputStream());
+ps=new PrintStream(c.getOutputStream()); 
+while(true){
+line=is.readLine();
+System.out.println("msg received and sent back to client"); 
+ps.println(line);
+}
+}
+catch(IOException e){
+System.out.println(e);
+}
+}
+}
